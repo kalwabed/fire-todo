1b41ed65bf9f657373d84f0786231d742d51e408
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2021-06-26 16:54:09 +0700

    feat(firebase): user signin/out

--- src/components/layout/TopNavigation.tsx
+++ src/components/layout/TopNavigation.tsx
@@ -1,9 +1,30 @@
+import { useState } from 'react'
 import { Box, Button, List, ListItem, Link as ChakraLink } from '@chakra-ui/react'
 import { Link } from '@reach/router'
 
 import Logo from '../../logo.svg'
+import { useActions, useStore } from 'src/store/hooks'
 
 const TopNavigation = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const isAuth = useStore(state => state.user.isAuth)
+  const signInWithGoogle = useActions(actions => actions.user.signInWithGoogle)
+  const signOutWithGoogle = useActions(actions => actions.user.signOutWithGoogle)
+
+  console.log(isAuth)
+
+  const handleButton = () => {
+    if (isAuth) {
+      setIsLoading(true)
+      signOutWithGoogle()
+      setIsLoading(false)
+      return
+    }
+    setIsLoading(true)
+    signInWithGoogle()
+    setIsLoading(false)
+  }
+
   return (
     <Box as="nav" height={['88px', '80px']} zIndex={50}>
       <List display="flex" flexWrap="wrap" alignItems="center" mx="auto" maxW={['2xl', '5xl']}>
@@ -40,7 +61,9 @@ const TopNavigation = () => {
         </ListItem>
 
         <ListItem display="flex" alignItems="center" h="full" ml={3}>
-          <Button colorScheme="teal">Login</Button>
+          <Button isLoading={isLoading} colorScheme="teal" onClick={handleButton}>
+            {isAuth ? 'Logout' : 'Login'}
+          </Button>
         </ListItem>
       </List>
     </Box>
--- src/store/user.ts
+++ src/store/user.ts
@@ -1,17 +1,58 @@
+import { action, Action, thunk, Thunk } from 'easy-peasy'
+
+import firebase from 'src/lib/firebase'
+
+export interface User {
+  email: string
+  name: string
+  uid: string
+}
+
 export interface UserModel {
-  user: {
-    isAuth: false
-    email: ''
-    username: ''
+  user?: User
+  isAuth: boolean
+  signInWithGoogle: Thunk<UserModel>
+  signOutWithGoogle: Thunk<UserModel>
+  saveUser: Action<UserModel, User>
+  changeIsAuth: Action<UserModel, boolean>
+  addUserToDB: Thunk<UserModel, User>
+}
+
+function formatUser(rawUser: firebase.auth.UserCredential): User {
+  return {
+    email: rawUser.user?.email || '',
+    uid: rawUser.user?.uid || '',
+    name: rawUser.user?.displayName || ''
   }
 }
 
 const userModel: UserModel = {
-  user: {
-    isAuth: false,
-    email: '',
-    username: ''
-  }
+  isAuth: false,
+  signInWithGoogle: thunk(async (actions, _) => {
+    const userSignIn = await firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider())
+    const user = formatUser(userSignIn)
+    actions.addUserToDB(user)
+    actions.saveUser(user)
+    actions.changeIsAuth(true)
+  }),
+  addUserToDB: thunk(async (_, payload) => {
+    await firebase
+      .firestore()
+      .collection('users')
+      .doc(payload.uid)
+      .set({ ...payload }, { merge: true })
+  }),
+  signOutWithGoogle: thunk(async (actions, _) => {
+    await firebase.auth().signOut()
+    actions.saveUser({ email: '', uid: '', name: '' })
+    actions.changeIsAuth(false)
+  }),
+  saveUser: action((state, payload) => {
+    state.user = { ...payload }
+  }),
+  changeIsAuth: action((state, payload) => {
+    state.isAuth = payload
+  })
 }
 
 export default userModel
