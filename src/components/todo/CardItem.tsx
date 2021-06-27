3265fbafd0345f2b330a2cd0b329538100b79f18
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2021-06-27 17:52:54 +0700

    feat: todos transition

--- src/components/todo/CardItem.tsx
+++ src/components/todo/CardItem.tsx
@@ -1,9 +1,11 @@
-import { Box, VStack, Text, Checkbox } from '@chakra-ui/react'
+import { useState } from 'react'
+import { Skeleton, Box, VStack, Text, Checkbox } from '@chakra-ui/react'
 
 import { useActions, useStore } from 'src/store/hooks'
 import { Todo } from 'src/store/todo'
 
 const CardItem = (todo: Todo) => {
+  const [isTodosTransition, setIsTodosTransition] = useState(false)
   const { createdAt, text, isCompleted } = todo
 
   const todos = useStore(state => state.todo.todos)
@@ -13,20 +15,25 @@ const CardItem = (todo: Todo) => {
 
   const handleCheckbox = () => {
     const refactoredTodos = todos.filter(target => target.id !== todo.id) as Todo[]
-
-    resetTodos(refactoredTodos)
-    updateTodo({ ...todo, isCompleted: !todo.isCompleted })
+    setIsTodosTransition(true)
+    setTimeout(() => {
+      resetTodos(refactoredTodos)
+      updateTodo({ ...todo, isCompleted: !todo.isCompleted })
+      setIsTodosTransition(false)
+    }, 1000)
   }
 
   return (
-    <Box border="1px" borderColor="gray.200" rounded="sm" p={2} _hover={{ borderColor: 'gray.400' }}>
-      <VStack align="start">
-        <Text color="gray.500">{createdAt.toString()}</Text>
-        <Checkbox defaultChecked={isCompleted} onChange={handleCheckbox}>
-          <Text as={isCompleted ? 'del' : 'span'}>{text}</Text>
-        </Checkbox>
-      </VStack>
-    </Box>
+    <Skeleton mb={1} isLoaded={!isTodosTransition}>
+      <Box border="1px" borderColor="gray.200" rounded="sm" p={2} _hover={{ borderColor: 'gray.400' }}>
+        <VStack align="start">
+          <Text color="gray.500">{createdAt.toString()}</Text>
+          <Checkbox defaultChecked={isCompleted} onChange={handleCheckbox}>
+            <Text as={isCompleted ? 'del' : 'span'}>{text}</Text>
+          </Checkbox>
+        </VStack>
+      </Box>
+    </Skeleton>
   )
 }
 
