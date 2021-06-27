aa07ca7aa633f3e668d254d5d785367e320dca2f
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2021-06-27 18:11:49 +0700

    feat: format date

--- src/components/todo/CardItem.tsx
+++ src/components/todo/CardItem.tsx
@@ -4,6 +4,11 @@ import { Skeleton, Box, VStack, Text, Checkbox } from '@chakra-ui/react'
 import { useActions, useStore } from 'src/store/hooks'
 import { Todo } from 'src/store/todo'
 
+function dateFormatter(date: string): string {
+  const fDate = new Date(date)
+  return `${fDate.toLocaleDateString()}, ${fDate.toLocaleTimeString()}`
+}
+
 const CardItem = (todo: Todo) => {
   const [isTodosTransition, setIsTodosTransition] = useState(false)
   const { createdAt, text, isCompleted } = todo
@@ -27,7 +32,7 @@ const CardItem = (todo: Todo) => {
     <Skeleton mb={1} isLoaded={!isTodosTransition}>
       <Box border="1px" borderColor="gray.200" rounded="sm" p={2} _hover={{ borderColor: 'gray.400' }}>
         <VStack align="start">
-          <Text color="gray.500">{createdAt.toString()}</Text>
+          <Text color="gray.500">{dateFormatter(createdAt)}</Text>
           <Checkbox defaultChecked={isCompleted} onChange={handleCheckbox}>
             <Text as={isCompleted ? 'del' : 'span'}>{text}</Text>
           </Checkbox>
